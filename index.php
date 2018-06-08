8d745d20ba4c632270e6c0e93a75ad7dd4780117
karolakrl <krolkarolina.kk@gmail.com>
2018-06-08 13:01:29 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Crawler</title>
+    <link rel="stylesheet" type="text/css" href="reset.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+  </head>
+  <body> 
+    <?php
+      echo "Hello World!";
+    ?>
+  </body>
+</html>
