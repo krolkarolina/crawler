8efecae76f52e241c9fe625505b0339607801287
karolakrl <krolkarolina.kk@gmail.com>
2018-06-08 17:19:07 +0200

    Changed name of files; Added form

---
+++ crawler.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Crawler</title>
+    <link rel="stylesheet" type="text/css" href="reset.css" />
+    <link rel="stylesheet" type="text/css" href="crawler.css" />
+  </head>
+  <body>
+    <div class="form">
+      <form class="form" action="crawler.php" method="get">
+        <p>Use field bellow to get all URL`s from web page:<p>
+        <div class="input">
+          <input type="text" name="urlAddress" placeholder="e.g: http://example.com">
+          <input type="submit" value="Submit">
+        </div>
+      </form>
+      <a href="crawler.php">reset</a>
+    </div>
+  </body>
+</html>
+
